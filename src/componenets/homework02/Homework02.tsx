5769772b96e629cf73fcd74ee729bd6ec9ccb741
Dace Liepina <dace.liepina77@gmail.com>
2025-10-16 17:14:54 +0200

    avatar

--- src/componenets/homework02/Homework02.tsx
+++ src/componenets/homework02/Homework02.tsx
@@ -5,21 +5,21 @@ import ProfileCard from "../ProfileCard/ProfileCard";
 export default function Homework02() {
 
     const user1 = {
-    avatar: "https://i.pravatar.cc/300",
+    avatar: "https://i.pravatar.cc/150?img=33",
     name: "John Lacks",
     job: "Teacher",
     hobby: "Traveling",
   };
 
   const user2 = {
-    avatar: "https://i.pravatar.cc/300",
+    avatar: "https://i.pravatar.cc/150?img=45",
     name: "Мaria Rosy",
     job: "Designer",
     hobby: "Drawing",
   };
 
   const user3 = {
-    avatar: "https://i.pravatar.cc/300",
+    avatar: "https://i.pravatar.cc/150?img=11",
     name: "Alejandro Outer",
     job: "Photograph",
     hobby: "Guitar",
